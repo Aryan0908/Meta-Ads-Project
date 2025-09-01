a09ec92d6ea7ed0a504492a8051ec2a1b4575197
Aryan0908 <68681552+Aryan0908@users.noreply.github.com>
2025-09-01 14:56:21 +0530

    Create 08_ctr_by_device.sql

---
+++ sql/08_ctr_by_device.sql
@@ -0,0 +1,13 @@
+SELECT 
+	s.device,
+	ROUND(AVG(ctr),2) AS avg_ctr
+FROM campaigns AS c
+JOIN adsets AS s
+	ON s.campaign_id = c.campaign_id
+JOIN ads AS ad
+	ON ad.adset_id = s.adset_id
+JOIN performance AS p
+	ON p.ad_id = ad.ad_id
+WHERE
+	c.objective = 'conversions'
+GROUP BY s.device
